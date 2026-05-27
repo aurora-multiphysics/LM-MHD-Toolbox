616440a1494de6d42a84963a8126979950f88657
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-05-27 16:44:37 +0100

    Continue adding Sloan solution; not yet tested

--- src/mhdtools/analytic.py
+++ src/mhdtools/analytic.py
@@ -379,6 +379,7 @@ class HuntII:
     def _calculate_scaled_B_field(self):
         self.scaled_B_field_z = self.scaled_H_field_z * self.permeability
 
+
 class Sloan:
     """Class for computing the Sloan and Smith 1966 analytic solution.
 
@@ -447,27 +448,33 @@ class Sloan:
         None.
 
         """
+        # Above: should change y to run from -(a+t_w) to (a+t_w),
+        # and implement B_2 solution
 
         self.Ha = Ha
         self.a = a
         self.b = b
-        self.t_w = t_w,
+        # self.t_w = t_w # don't think this is needed
+        self.q = 1 + t_w / a  # a+tw=aq, so q=(a+tw)/a = 1+tw/a
         self.truncation = truncation
-        if isinstance(x, (float, np.floating)) and isinstance(x, (float, np.floating)):
+        if isinstance(x, (float, np.floating)) and isinstance(
+            x, (float, np.floating)
+        ):
             self.x = [x]
             self.y = [y]
             self.single_point = True
         elif type(x) is not np.ndarray or type(y) is not np.ndarray:
             raise Exception(
-                "Either both x and y must be numpy arrays, or both must be floats"
+                "Either both x and y must be numpy arrays, "
+                "or both must be floats"
             )
         else:
             self.x = x
             self.y = y
             self.single_point = False
         self.dyn_visc = dyn_visc
-        self.conductivity_w = conductivity_w,
-        self.conductivity_f = conductivity_f,
+        self.conductivity_w = (conductivity_w,)
+        self.conductivity_f = (conductivity_f,)
         self.permeability = permeability
 
         self.r = self.b / self.a
@@ -482,174 +489,150 @@ class Sloan:
         """Calculate the nondimensional velocity, magnetic field,
         flow rate and average velocity."""
         n_list = list(range(0, self.truncation))
-        v = np.zeros(self.xyShape)
-        h = np.zeros(self.xyShape)
+        w = np.zeros(self.xyShape)
+        B = np.zeros(self.xyShape)
         Q = 0
 
         for n in n_list:
-            # calculate term in V equation
-            # calculate term in H equation
-            # add terms to V and H equations
+            # # calculate term in V equation
+            # # calculate term in H equation
+            # # add terms to V and H equations
 
             # calculate common terms
             a_n = self._an(n)
-    #         N_k = self._Nk(alpha_k)
+            k_n = self._kn(n, a_n)
             alpha_n = self._alphan(n, a_n)
             beta_n = self._betan(n, a_n)
 
-    #         vXi_k = np.zeros(self.xyShape[0])
-    #         vEta_k = np.zeros(self.xyShape[1])
+            wXi_n = np.zeros(self.xyShape[0])
+            wEta_n = np.zeros(self.xyShape[1])
 
-    #         hXi_k = np.zeros(self.xyShape[0])
-    #         hEta_k = np.zeros(self.xyShape[1])
+            BXi_n = np.zeros(self.xyShape[0])
+            BEta_n = np.zeros(self.xyShape[1])
 
-    #         # compute xXi constant part
-    #         vXi_constant = self._vXiConstant(k, alpha_k)
+            # compute xXi variable part
+            for i in range(len(self.xXi)):
+                xi_val = self.xXi[i]
+                wXi_n[i] = self._XiComponent(a_n, xi_val)
+            BXi_n = wXi_n
 
-    #         # compute xXi variable part
-    #         for n in range(len(self.xXi)):
-    #             xi_val = self.xXi[n]
-    #             vXi_k[n] = self._vXiComponent(vXi_constant, alpha_k, xi_val)
-    #         hXi_k = vXi_k
+            # compute yEta constant parts
+            D_n = self._Dn(a_n, beta_n)
+            E_n = self._En(a_n, alpha_n)
+            Eta_n_denom = self._Etan_denom(D_n, E_n, alpha_n, beta_n)
 
-    #         # compute yEta constant parts
-    #         term2_k_constant = self._term2kConstant(r1_k, r2_k, N_k)
-    #         term3_k_constant = self._term3kConstant(r1_k, r2_k, N_k)
+            for i in range(len(self.yEta)):
+                eta_val = self.yEta[i]
+                wEta_n[i] = self._wEtaComponent(
+                    eta_val, D_n, E_n, alpha_n, beta_n, Eta_n_denom
+                )
+                BEta_n[i] = self._BEtaComponent(
+                    eta_val, D_n, E_n, alpha_n, beta_n, Eta_n_denom
+                )
 
-    #         # compute yEta variable parts
-    #         for n in range(len(self.yEta)):
-    #             eta_val = self.yEta[n]
+            # compute iteration
+            w_n = k_n * np.outer(wEta_n, wXi_n)
+            B_n = k_n * np.outer(BEta_n, BXi_n)
 
-    #             v2 = self._v2k(r1_k, eta_val, term2_k_constant)
-    #             v3 = self._v3k(r2_k, eta_val, term3_k_constant)
-    #             vEta_k[n] = self._vEtak(v2, v3)
+            # add to series
+            w += w_n
+            B += B_n
 
-    #             h2 = self._h2k(r1_k, eta_val, term2_k_constant)
-    #             h3 = self._h3k(r2_k, eta_val, term3_k_constant)
-    #             hEta_k[n] = self._hEtak(h2, h3)
+        #         # calculate Q_k
+        #         vXi_integral = self._vXiIntegral(alpha_k)
+        #         v2_k_integral = self._v2kIntegral(r1_k)
+        #         v3_k_integral = self._v3kIntegral(r2_k)
+        #         Q_Xi_component = vXi_constant * vXi_integral
+        #         Q_v2 = term2_k_constant * v2_k_integral
+        #         Q_v3 = term3_k_constant * v3_k_integral
+        #         Q_Eta_component = self._QEtak(Q_v2, Q_v3)
+        #         Q_k = Q_Xi_component * Q_Eta_component
 
-    #         # compute outer product of orthogonal components
-    #         v_k = np.outer(vXi_k, vEta_k)
-    #         h_k = np.outer(hXi_k, hEta_k)
+        #         Q += Q_k
 
-    #         v += v_k
-    #         h += h_k
+        w = np.transpose(w)
+        B = np.transpose(B)
 
-    #         # calculate Q_k
-    #         vXi_integral = self._vXiIntegral(alpha_k)
-    #         v2_k_integral = self._v2kIntegral(r1_k)
-    #         v3_k_integral = self._v3kIntegral(r2_k)
-    #         Q_Xi_component = vXi_constant * vXi_integral
-    #         Q_v2 = term2_k_constant * v2_k_integral
-    #         Q_v3 = term3_k_constant * v3_k_integral
-    #         Q_Eta_component = self._QEtak(Q_v2, Q_v3)
-    #         Q_k = Q_Xi_component * Q_Eta_component
+        if self.single_point:
+            w = float(w)
+            B = float(B)
 
-    #         Q += Q_k
-
-    #     v = np.transpose(v)
-    #     h = np.transpose(h)
-
-    #     if self.single_point:
-    #         v = float(v)
-    #         h = float(h)
-
-    #     self.v = v
-    #     self.h = h
-    #     self.Q = Q
-    #     self.average_velocity = self.Q / (4 * self.l_ratio)
-    #     self._solved = True
+        self.w = w
+        self.B = B
+        self.Q = Q
+        # self.average_velocity = self.Q / (4 * self.l_ratio)
+        # self._solved = True
 
     def _an(self, n):
         a_n = (n + 0.5) * np.pi / self.r
         return a_n
 
-    # def _Nn(self, a_n):
-    #     N_n = np.sqrt(self.Ha**2 + 4 * a_n**2)
-    #     return N_n
+    def _kn(self, n, a_n):
+        k_n = 2 * ((-1) ** n) / (self.r * a_n**3)
+        return k_n
 
     def _alphan(self, n, a_n):
         alpha_n = 0.5 * (-self.Ha + np.sqrt(self.Ha**2 + 4 * a_n**2))
         return alpha_n
-    
+
     def _betan(self, n, a_n):
         beta_n = 0.5 * (-self.Ha - np.sqrt(self.Ha**2 + 4 * a_n**2))
         return beta_n
 
-    # def _vXiConstant(self, k, alpha_k):
-    #     vXi_constant = 2 * ((-1) ** k) / (self.l_ratio * alpha_k**3)
-    #     return vXi_constant
+    def _XiComponent(self, a_n, xi_val):
+        return np.cos(a_n * xi_val)
 
-    # def _vXiComponent(self, vXi_constant, alpha_k, xi_val):
-    #     vXi_variable = np.cos(alpha_k * xi_val)
-    #     vXi_component = vXi_constant * vXi_variable
-    #     return vXi_component
+    def _Dn(self, a_n, beta_n):
+        sigma_1 = self.conductivity_f
+        sigma_2 = self.conductivity_w
+        D_n = sigma_1 * a_n * np.sinh(beta_n) - sigma_2 * beta_n * np.tanh(
+            a_n * (1 - self.q)
+        ) * np.cosh(beta_n)
+        return D_n
 
-    # def _vEtak(self, v2, v3):
-    #     vEta_component_k = 1 - v2 - v3
-    #     return vEta_component_k
+    def _En(self, a_n, alpha_n):
+        sigma_1 = self.conductivity_f
+        sigma_2 = self.conductivity_w
+        E_n = sigma_1 * a_n * np.sinh(alpha_n) - sigma_2 * alpha_n * np.tanh(
+            a_n * (1 - self.q)
+        ) * np.cosh(alpha_n)
+        return E_n
 
-    # def _hEtak(self, h2, h3):
-    #     hEta_component_k = h2 - h3
-    #     return hEta_component_k
+    def _Etan_denom(D_n, E_n, alpha_n, beta_n):
+        denom = D_n * np.cosh(alpha_n) - E_n * np.cosh(beta_n)
+        return denom
 
-    # def _term2kConstant(self, r1_k, r2_k, N_k):
-    #     dB = self.dB
-    #     if dB == np.inf:
-    #         term2_k_constant = r2_k / (N_k * (1 + np.exp(-2 * r1_k)))
-    #         return term2_k_constant
-    #     else:
-    #         num = (dB * r2_k + ((1 - np.exp(-2 * r2_k)) / (1 + np.exp(-2 * r2_k)))) / 2
-    #         den = (((1 + np.exp(-2 * r1_k)) / 2) * dB * N_k) + (
-    #             (1 + np.exp(-2 * (r1_k + r2_k))) / (1 + np.exp(-2 * r2_k))
-    #         )
-    #         term2_k_constant = num / den
-    #         return term2_k_constant
+    def _wEtaComponent(self, eta_val, D_n, E_n, alpha_n, beta_n, Eta_n_denom):
+        if 1 < eta_val <= self.q:
+            return 0
+            # w=0 B2 h
+        elif -1 <= eta_val <= 1:
+            numer = D_n * np.cosh(alpha_n * eta_val) - E_n * np.cosh(
+                beta_n * eta_val
+            )
+            wEtaComponent = 1 - (numer / Eta_n_denom)
+            return wEtaComponent
+        elif -self.q <= eta_val < -1:
+            # w=0 B2 s
+            return 0
+        else:
+            raise ValueError(f"eta={eta_val} is outside defined domain")
 
-    # def _v2k(self, r1_k, eta_val, term2_k_constant):
-    #     v2_k_variable = np.exp(-r1_k * (1 - eta_val)) + np.exp(-r1_k * (1 + eta_val))
-    #     v2_k = term2_k_constant * v2_k_variable
-    #     return v2_k
-
-    # def _h2k(self, r1_k, eta_val, term2_k_constant):
-    #     h2_k_variable = np.exp(-r1_k * (1 - eta_val)) - np.exp(-r1_k * (1 + eta_val))
-    #     h2_k = term2_k_constant * h2_k_variable
-    #     return h2_k
-
-    # def _term3kConstant(self, r1_k, r2_k, N_k):
-    #     dB = self.dB
-    #     if dB == np.inf:
-    #         term3_k_constant = r1_k / (N_k * (1 + np.exp(-2 * r2_k)))
-    #         return term3_k_constant
-    #     else:
-    #         num = (dB * r1_k + ((1 - np.exp(-2 * r1_k)) / (1 + np.exp(-2 * r1_k)))) / 2
-    #         den = (((1 + np.exp(-2 * r2_k)) / 2) * dB * N_k) + (
-    #             (1 + np.exp(-2 * (r1_k + r2_k))) / (1 + np.exp(-2 * r1_k))
-    #         )
-    #         term3_k_constant = num / den
-    #         return term3_k_constant
-
-    # def _v3k(self, r2_k, eta_val, term3_k_constant):
-    #     v3_k_variable = np.exp(-r2_k * (1 - eta_val)) + np.exp(-r2_k * (1 + eta_val))
-    #     v3_k = term3_k_constant * v3_k_variable
-    #     return v3_k
-
-    # def _h3k(self, r2_k, eta_val, term3_k_constant):
-    #     h3_k_variable = np.exp(-r2_k * (1 - eta_val)) - np.exp(-r2_k * (1 + eta_val))
-    #     h3_k = term3_k_constant * h3_k_variable
-    #     return h3_k
-
-    # def _vXiIntegral(self, alpha_k):
-    #     v_Xi_integral = (2 / alpha_k) * np.sin(alpha_k * self.l_ratio)
-    #     return v_Xi_integral
-
-    # def _v2kIntegral(self, r1_k):
-    #     v2_k_integral = (2 / r1_k) * (1 - np.exp(-2 * r1_k))
-    #     return v2_k_integral
-
-    # def _v3kIntegral(self, r2_k):
-    #     v3_k_integral = (2 / r2_k) * (1 - np.exp(-2 * r2_k))
-    #     return v3_k_integral
+    def _BEtaComponent(self, eta_val, D_n, E_n, alpha_n, beta_n, Eta_n_denom):
+        if 1 < eta_val <= self.q:
+            return 0
+            # w=0 B2 h
+        elif -1 <= eta_val <= 1:
+            numer = D_n * np.cosh(alpha_n * eta_val) - E_n * np.cosh(
+                beta_n * eta_val
+            )
+            wEtaComponent = 1 - (numer / Eta_n_denom)
+            return wEtaComponent
+        elif -self.q <= eta_val < -1:
+            # w=0 B2 s
+            return 0
+        else:
+            raise ValueError(f"eta={eta_val} is outside defined domain")
 
     # def _QEtak(self, Q_v2, Q_v3):
     #     Q_Eta_component_k = 2 - Q_v2 - Q_v3
@@ -755,6 +738,7 @@ class Sloan:
 
     # def _calculate_scaled_B_field(self):
     #     self.scaled_B_field_z = self.scaled_H_field_z * self.permeability
+
 
 def makeXYVectors(N_x, N_y, a, b):
     x = np.linspace(-b, b, N_x)
