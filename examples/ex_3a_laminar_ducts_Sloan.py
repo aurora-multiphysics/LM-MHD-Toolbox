f2ec71876c015049260fc7deeca8b1d32e6eaee6
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-06-24 11:32:17 +0100

    Add Hunt and Sloan solution comparison

--- examples/ex_3a_laminar_ducts_Sloan.py
+++ examples/ex_3a_laminar_ducts_Sloan.py
@@ -3,20 +3,175 @@ import mhdtools.analytic
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Define functions for Hunt and Sloan solutions
+
+
+def Hunt_solution(
+    Ha,
+    a,
+    b,
+    t_w,
+    truncation,
+    N_x,
+    N_y,
+    dyn_visc,
+    conductivity_f,
+    conductivity_w,
+    permeability,
+    average_velocity,
+):
+    # Create the grid
+
+    # x, y = mhdtools.analytic.makeXYVectors(N_x, N_y, a + t_w, b)
+    x, y = mhdtools.analytic.makeXYVectors(N_x, N_y, a, b)
+
+    # calculate conductivity ratio
+    dB = (t_w * conductivity_w) / (
+        a * conductivity_f
+    )  # Hartmann wall conduction ratio
+
+    # Create the case
+    hunt_case = mhdtools.analytic.HuntII(
+        Ha,
+        a,
+        b,
+        dB,
+        truncation,
+        x,
+        y,
+        dyn_visc,
+        conductivity_f,
+        permeability,
+    )
+
+    # Solve the case
+    hunt_case.analytic_solve()
+
+    # Constrain average velocity
+    hunt_case.set_scaled_average_velocity(average_velocity)
+
+    # Calculate scaled fields
+    hunt_case.calculate_scaled_solution()
+
+    return x, y, hunt_case
+
+
+def Sloan_solution(
+    Ha,
+    a,
+    b,
+    t_w,
+    truncation,
+    N_x,
+    N_y,
+    dyn_visc,
+    conductivity_f,
+    conductivity_w,
+    permeability,
+    average_velocity,
+    fluid_only=False,
+):
+    # Create the grid
+
+    if fluid_only:
+        x, y = mhdtools.analytic.makeXYVectors(N_x, N_y, a, b)
+    else:
+        x, y = mhdtools.analytic.makeXYVectors(N_x, N_y, a + t_w, b)
+
+    # Create the case
+    sloan_case = mhdtools.analytic.Sloan(
+        Ha,
+        a,
+        b,
+        t_w,
+        truncation,
+        x,
+        y,
+        dyn_visc,
+        conductivity_f,
+        conductivity_w,
+        permeability,
+    )
+
+    # Solve the case
+    sloan_case.analytic_solve()
+
+    # Constrain average velocity
+    sloan_case.set_scaled_average_velocity(average_velocity)
+
+    # Calculate scaled fields
+    sloan_case.calculate_scaled_solution()
+
+    return x, y, sloan_case
+
+
+def compare_solutions(
+    Ha,
+    a,
+    b,
+    t_w,
+    truncation,
+    N_x,
+    N_y,
+    dyn_visc,
+    conductivity_f,
+    conductivity_w,
+    permeability,
+    average_velocity,
+):
+    # This function can only compare the fluid domain as the HuntII solution
+    # doesn't include solid domains
+
+    x, y, hunt_case = Hunt_solution(
+        Ha,
+        a,
+        b,
+        t_w,
+        truncation,
+        N_x,
+        N_y,
+        dyn_visc,
+        conductivity_f,
+        conductivity_w,
+        permeability,
+        average_velocity,
+    )
+
+    x, y, sloan_case = Sloan_solution(
+        Ha,
+        a,
+        b,
+        t_w,
+        truncation,
+        N_x,
+        N_y,
+        dyn_visc,
+        conductivity_f,
+        conductivity_w,
+        permeability,
+        average_velocity,
+        fluid_only=True,
+    )
+
+    diff_uz = sloan_case.scaled_velocity_z - hunt_case.scaled_velocity_z
+
+    return x, y, diff_uz
+
+
 # Create a grid
 
 a = 1  # height
 b = 1  # width
 N_x = 501  # num points in width
 N_y = 501  # num points in height
-t_w = 0.1  # Hartmann wall thickness
+t_w = 1  # Hartmann wall thickness
 
-x, y = mhdtools.analytic.makeXYVectors(N_x, N_y, a + t_w, b)
-
-# Define Hunt flow (finite conductivity walls) case
-# Based on HuntII case (side walls are always perfectly insulating)
-# Using Sloan 66 solution for thick walls
-# Assumed to be flow along z in a square duct
+"""
+Define a Hunt-II style flow with finite conductivity Hartmann walls
+and perfectly insulating side walls, but considering the case of
+thick walls. Hunt's solution assumes thin walls, but the Sloan 1966
+solution includes the effect of Hartmann wall thickness.
+"""
 
 Ha = 500  # Hartmann number
 truncation = 70  # Number of Fourier iterations
@@ -26,43 +181,88 @@ conductivity_w = 1  # Solid conductivity
 permeability = 1  # Fluid permeability
 average_velocity = 1  # Average flow velocity
 
-# Create the case
-sloan_case = mhdtools.analytic.Sloan(
+x, y, sloan_case = Sloan_solution(
     Ha,
     a,
     b,
     t_w,
     truncation,
-    x,
-    y,
+    N_x,
+    N_y,
     dyn_visc,
     conductivity_f,
     conductivity_w,
     permeability,
+    average_velocity,
 )
 
-# Solve the case
-sloan_case.analytic_solve()
-
-# Constrain average velocity
-
-sloan_case.set_scaled_average_velocity(average_velocity)
-
-# Calculate scaled fields
-sloan_case.calculate_scaled_solution()
-sloan_case_uz = sloan_case.scaled_velocity_z
-sloan_case_bz = sloan_case.scaled_B_field_z
-sloan_case_K = sloan_case.scaled_pressure_grad
-
 fig = plt.figure(figsize=(10, 8))
-ax1 = plt.pcolormesh(x, y, sloan_case_uz, cmap="coolwarm", shading="gouraud")
+ax1 = plt.pcolormesh(
+    x, y, sloan_case.scaled_velocity_z, cmap="coolwarm", shading="gouraud"
+)
 plt.xlabel("x")
 plt.ylabel("y")
 cb = plt.colorbar()
 fig.tight_layout()
-plt.savefig("ex_3a_laminar_ducts_HuntII_Sloan.png")
-print("Pressure Drop K = %f [ND]" % sloan_case_K)
-print("Pressure Drop K = %f Pa/m" % sloan_case.scaled_pressure_grad)
+plt.savefig("ex_3a_laminar_ducts_Sloan.png")
+print("\nSloan:")
+print("Pressure Gradient -dp/dz = %f Pa/m" % sloan_case.scaled_pressure_grad)
 print("Flow Rate Q = %f [ND]" % sloan_case.Q)
 print("Mean Velocity = %f [ND]" % sloan_case.average_velocity)
 print("Rescaled Mean Velocity = %f m/s" % sloan_case.scaled_average_velocity)
+print("Maximum Velocity = %f m/s" % np.max(sloan_case.scaled_velocity_z))
+
+
+x, y, hunt_case = Hunt_solution(
+    Ha,
+    a,
+    b,
+    t_w,
+    truncation,
+    N_x,
+    N_y,
+    dyn_visc,
+    conductivity_f,
+    conductivity_w,
+    permeability,
+    average_velocity,
+)
+
+fig = plt.figure(figsize=(10, 8))
+ax1 = plt.pcolormesh(
+    x, y, hunt_case.scaled_velocity_z, cmap="coolwarm", shading="gouraud"
+)
+plt.xlabel("x")
+plt.ylabel("y")
+cb = plt.colorbar()
+fig.tight_layout()
+plt.savefig("ex_3a_laminar_ducts_HuntII.png")
+print("\nHunt:")
+print("Pressure Gradient -dp/dz = %f Pa/m" % hunt_case.scaled_pressure_drop)
+print("Flow Rate Q = %f [ND]" % hunt_case.Q)
+print("Mean Velocity = %f [ND]" % hunt_case.average_velocity)
+print("Rescaled Mean Velocity = %f m/s" % hunt_case.scaled_average_velocity)
+print("Maximum Velocity = %f m/s" % np.max(hunt_case.scaled_velocity_z))
+
+x, y, uz_diff = compare_solutions(
+    Ha,
+    a,
+    b,
+    t_w,
+    truncation,
+    N_x,
+    N_y,
+    dyn_visc,
+    conductivity_f,
+    conductivity_w,
+    permeability,
+    average_velocity,
+)
+
+fig = plt.figure(figsize=(10, 8))
+ax1 = plt.pcolormesh(x, y, uz_diff, cmap="coolwarm", shading="gouraud")
+plt.xlabel("x")
+plt.ylabel("y")
+cb = plt.colorbar()
+fig.tight_layout()
+plt.savefig("ex_3a_laminar_ducts_diff.png")
